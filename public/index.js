630577c12c5c48633fd1e1f96ba12341e93d1506
Peter Skopal <pesk@sto-m-pesk.lan>
2019-03-02 22:57:12 +0100

    feat: add extra cases, and time logic.

--- public/index.js
+++ public/index.js
@@ -1,20 +1,46 @@
-var countDownDate = new Date("Mar 12, 2019 00:00:00").getTime();
+Number.prototype.pad = function(n) {
+  if (n == undefined) n = 2;
 
-var x = setInterval(function() {
-  var now = new Date().getTime();
-  var distance = countDownDate - now;
+  return (new Array(n).join("0") + this).slice(-n);
+};
 
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+let now = new Date().getTime();
+var specificDate = "Mar 12";
+var year = new Date().getFullYear();
+var annualDate = false;
+let countDownDate = new Date(`${specificDate}, ${year} 00:00:00`);
+if (annualDate) setupCountDownDate(year);
+countDown(annualDate);
+var interval = setInterval(countDown, 1000);
 
-  document.getElementById(
-    "countDown"
-  ).innerHTML = `${days} days \& ${hours}:${minutes}:${seconds}`;
-
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("countDown").innerHTML = "Count down expired";
+function setupCountDownDate(year) {
+  while (countDownDate - now < 0) {
+    year++;
+    countDownDate = new Date(`${specificDate}, ${year} 00:00:00`);
   }
-}, 1000);
+}
+
+function countDown() {
+  now = new Date();
+  var distance = countDownDate.getTime() - now.getTime();
+
+  if (countDownDate.getDate() === now.getDate()) {
+    document.getElementById("countDown").innerHTML =
+      "&#x1F389 It's Today! &#x1F389";
+  } else if (!annualDate && distance < 0) {
+    clearInterval(interval);
+    document.getElementById("countDown").innerHTML =
+      "Already passed... &#x1F614";
+  } else {
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    ).pad();
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)).pad();
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000).pad();
+
+    document.getElementById(
+      "countDown"
+    ).innerHTML = `${days} days \& ${hours}:${minutes}:${seconds}`;
+  }
+}
